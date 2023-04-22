7b815606edeaa25862e5a312b3e857f4300fe4ac
Tom Mi <tommi@niemi.lol>
2023-04-22 20:23:56 +0000

    fix: remove help causing error as nonexistent (#23)
    style: typo
    
    docs: corrected plugin spec location (#25)
    
    Also fixed minor typos and ambiguities.
    
    docs: simplify cmp-emoji example
    
    fix: on_attach deprecated, replace with lsp.on_attach (#45)
    
    fix: removed some outdated examples
    
    fix: Deperecated syntax in bootstrapping of LazyVim starter (#56)
    
    In the current state of the lazy.nvim repository, certain updates have been made. However, there remains an outdated reference to 'vim.loop' for the bootstrapping process of 'lazyvim' in this start repo for LazyVim, despite 'vim.loop' being deprecated. To rectify this, I suggest a minor alteration by replacing it with the following code snippet:
    
    ```lua
    if not (vim.uv or vim.loop).fs_stat(lazypath) then
        -- bootstrap
    do
    ```
    
    docs: Update example plugin file to use native snippets with supertab. (#66)
    
    fix: removed unnecessary env var (#67)
    
    according to https://github.com/LazyVim/LazyVim/issues/2063#issuecomment-2143841592
    this is not needed
    
    fix: improve comment about extras loading in config (#75)
    
    * fix: improve comment about extras loading in config
    
    * fix: use LazyExtras
    
    ---------
    
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>
    
    docs: removed supertab example
    
    fix: add error handling to initial clone
    
    Rebase - Add custom configuration

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Sync native editor registers with your system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Disable all legacy external language providers to boost startup speed
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_python_provider  = 0 -- Disables legacy Python 2 check
+vim.g.loaded_node_provider    = 0
+vim.g.loaded_perl_provider    = 0
+vim.g.loaded_ruby_provider    = 0
+
