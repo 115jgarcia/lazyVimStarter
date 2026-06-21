236b9e242347b5ef12f68f61309c3b79f4479da5
Jose Garcia <156558069+115jgarcia@users.noreply.github.com>
2026-06-21 10:23:40 -0400

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,12 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+}
+
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
